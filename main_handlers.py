72eee48bb76140576cd492fea7ba30ebce9d2a65
bsehgal <bsehgal@okaki.com>
2025-06-14 14:18:01 -0600

    having clear folder and script name conventions for better understading

--- main_handlers.py
+++ main_handlers.py
@@ -7,11 +7,14 @@ from utils.run_utils import make_run_dir, get_pipeline_io_paths
 logger = logging.getLogger(__name__)
 
 
-def _handle_detect_command(args, pipeline_config: PipelineConfig, global_config: GlobalConfig):
-    from detect_pose import run_detection_pipeline
+def _handle_detect_command(
+    args, pipeline_config: PipelineConfig, global_config: GlobalConfig
+):
+    from detection_pipeline import run_detection_pipeline
 
     input_dir, base_pipeline_out = get_pipeline_io_paths(
-        global_config.paths, pipeline_config.paths.dataset)
+        global_config.paths, pipeline_config.paths.dataset
+    )
     run_dir = make_run_dir(
         base_out=base_pipeline_out,
         pipeline_name=args.pipeline_name,
@@ -32,15 +35,18 @@ def _handle_detect_command(args, pipeline_config: PipelineConfig, global_config:
         pipeline_config=pipeline_config,
         global_config=global_config,
         input_dir=video_folder,
-        output_dir=step_out
+        output_dir=step_out,
     )
 
 
-def _handle_noise_command(args, pipeline_config: PipelineConfig, global_config: GlobalConfig):
+def _handle_noise_command(
+    args, pipeline_config: PipelineConfig, global_config: GlobalConfig
+):
     from noise.simulator import NoiseSimulator
 
     input_dir, base_pipeline_out = get_pipeline_io_paths(
-        global_config.paths, pipeline_config.paths.dataset)
+        global_config.paths, pipeline_config.paths.dataset
+    )
     run_dir = make_run_dir(
         base_out=base_pipeline_out,
         pipeline_name=args.pipeline_name,
@@ -60,8 +66,10 @@ def _handle_noise_command(args, pipeline_config: PipelineConfig, global_config: 
     simulator.process_all_videos(str(input_folder), str(output_folder))
 
 
-def _handle_filter_command(args, pipeline_config: PipelineConfig, global_config: GlobalConfig):
-    from filter_runner import run_keypoint_filtering_from_config
+def _handle_filter_command(
+    args, pipeline_config: PipelineConfig, global_config: GlobalConfig
+):
+    from filter_pipeline import run_keypoint_filtering_from_config
 
     input_dir, base_pipeline_out = get_pipeline_io_paths(
         global_config.paths, pipeline_config.paths.dataset
@@ -93,11 +101,14 @@ def _handle_filter_command(args, pipeline_config: PipelineConfig, global_config:
     )
 
 
-def _handle_assess_command(args, pipeline_config: PipelineConfig, global_config: GlobalConfig):
-    from assessment_runner import run_pose_assessment_pipeline
+def _handle_assess_command(
+    args, pipeline_config: PipelineConfig, global_config: GlobalConfig
+):
+    from evaluation_pipeline import run_pose_assessment_pipeline
 
     _, base_pipeline_out = get_pipeline_io_paths(
-        global_config.paths, pipeline_config.paths.dataset)
+        global_config.paths, pipeline_config.paths.dataset
+    )
     run_dir = make_run_dir(
         base_out=base_pipeline_out,
         pipeline_name=args.pipeline_name,
@@ -110,5 +121,4 @@ def _handle_assess_command(args, pipeline_config: PipelineConfig, global_config:
     step_out = run_dir
     step_out.mkdir(parents=True, exist_ok=True)
 
-    run_pose_assessment_pipeline(
-        pipeline_config, global_config, output_dir=step_out)
+    run_pose_assessment_pipeline(pipeline_config, global_config, output_dir=step_out)
