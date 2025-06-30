42b839c00d458e8437d4ffaf6dce78d166508439
bsehgal <bsehgal@okaki.com>
2025-06-30 01:56:31 -0600

    testing pipeline variants

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def run_pipeline(pipelines_yaml_path: str = "config_yamls/pipelines.yaml"):
+def run_pipeline(pipelines_yaml_path: str = "config_yamls/testing_pipelines.yaml"):
     with open(pipelines_yaml_path, "r") as f:
         config = yaml.safe_load(f)
 
