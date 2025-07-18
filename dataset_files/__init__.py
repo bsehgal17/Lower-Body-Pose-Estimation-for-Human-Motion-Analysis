0f23183da406a5c92fee2a84a2985df90f45295c
bsehgal <bsehgal@okaki.com>
2025-07-18 01:29:54 -0600

    added movi script files

--- dataset_files/__init__.py
+++ dataset_files/__init__.py
@@ -1,6 +1,13 @@
-# This file makes 'dataset_files' a Python package and allows direct imports from HumanEva.
+# dataset_files/__init__.py
 
+# HumanEva imports
 from .HumanEva.get_gt_keypoint import *
 from .HumanEva.humaneva_evaluation import *
 from .HumanEva.humaneva_metadata import *
 from .HumanEva.visualize_gt_pred import *
+
+# MoVi imports
+from .MoVi.movi_gt_loader import *
+from .MoVi.movi_evaluation import *
+from .MoVi.movi_metadata import *
+from .MoVi.movi_assessor import *
