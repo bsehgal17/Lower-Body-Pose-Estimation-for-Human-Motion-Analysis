0bfc081b6bc7d825e285258f54f78c25f3443b9f
bsehgal <bsehgal@okaki.com>
2025-08-15 18:09:48 -0600

    analyzing data at higher threshold

--- Analysis scripts/HumanEva_config.py
+++ Analysis scripts/HumanEva_config.py
@@ -2,20 +2,20 @@ from types import SimpleNamespace
 
 # --- File Paths ---
 VIDEO_DIRECTORY = "/storage/Projects/Gaitly/bsehgal/lower_body_pose_est/HumanEva"
-PCK_FILE_PATH = "/storage/Projects/Gaitly/bsehgal/lower_body_pose_est/pipeline_results/HumanEva/detect_RTMW/evaluation/2025-08-12_13-42-13/2025-07-29_13-12-55_metrics.xlsx"
+PCK_FILE_PATH = "/storage/Projects/Gaitly/bsehgal/lower_body_pose_est/pipeline_results/HumanEva/detect_RTMW/evaluation/2025-08-15_17-49-17/2025-07-29_13-12-55_metrics.xlsx"
 SAVE_FOLDER = "/storage/Projects/Gaitly/bsehgal/lower_body_pose_est/pipeline_results/analysis_results/HumanEva"
 DATASET_NAME = "humaneva"
 # --- PCK Data Columns ---
 PCK_OVERALL_SCORE_COLUMNS = [
-    'overall_overall_pck_0.01',
-    'overall_overall_pck_0.02',
-    'overall_overall_pck_0.05'
+    'overall_overall_pck_0.10',
+    'overall_overall_pck_0.20',
+    'overall_overall_pck_0.50'
 ]
 
 PCK_PER_FRAME_SCORE_COLUMNS = [
-    'pck_per_frame_pck_0.01',
-    'pck_per_frame_pck_0.02',
-    'pck_per_frame_pck_0.05'
+    'pck_per_frame_pck_0.10',
+    'pck_per_frame_pck_0.20',
+    'pck_per_frame_pck_0.50'
 ]
 
 # --- Columns for Video Mapping ---
